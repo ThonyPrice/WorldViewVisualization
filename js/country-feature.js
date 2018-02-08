f4e300b4b5645ad77f9c15ad5a69bc5e61d7fb41
Thony Price <thonyprice@gmail.com>
2018-02-08 21:40:45 +0100

    No changes

--- js/country-feature.js
+++ js/country-feature.js
@@ -1,6 +1,17 @@
+var current_country = "Sweden";
+
 function printCountry(d, i) {
-  console.log(d.properties.name);
-  document.
-    getElementById("click-feature").
-    innerHTML = d.properties.name;
+
+  var country = d.properties.name;
+  console.log("You clicked: ", country);
+  document.getElementById("click-feature").innerHTML = country;
+
+  d3.json("/data/happiness95.json", function(data) {
+    for (var key in data) {
+        if (data[key].Country == country) {
+            console.log(data[key]);
+        }
+    }
+  });
+
 }
